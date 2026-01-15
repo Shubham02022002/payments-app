7b4ca03144dea360df8e90c8bee024cdc7df7588
Shubham02022002 <chaudhary2001shubham@gmail.com>
2026-01-15 16:00:56 +0530

    +Signin,signup cards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+import Transfer from "./components/Transfer";
+import Dashboard from "./components/Dashboard";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <div>Payment App</div>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<Transfer />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
