a82535130bc23f028dc73d27ae3facbe1acc885b
Shubham02022002 <chaudhary2001shubham@gmail.com>
2026-08-15 11:51:55 +0530

    + PORT

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,18 @@ const express = require("express");
 const connectDb = require("./db.js");
 const User = require("./models/userSchema.js");
 const userRouter = require("./routes/userRoutes.js");
-const cors = require("cors")
+const cors = require("cors");
 const router = require("./router.js");
 const app = express();
+require("dotenv/config");
 
-
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 connectDb();
 
 app.use("/api/v1", router);
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0",() => {
   console.log(`server is up`);
 });
